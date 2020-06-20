809aa0bc6ba952dd12fe789f2d5196fa6f51e7b3
JonMar86 <66481955+JonMar86@users.noreply.github.com>
2020-06-20 09:29:19 -0400

    - Unit tests for QuoteComparer

---
+++ QuoteManager/QuoteManager/QuoteManagerTests/QuoteComparerTests.cs
@@ -0,0 +1,100 @@
+﻿namespace QuoteManagerTests
+{
+    using NUnit.Framework;
+    using Quoter;
+    using Quoter.Core;
+    using System;
+
+    [TestFixture]
+    internal class QuoteComparerTests
+    {
+        [Test]
+        public void Compare_SameObject_ReturnsEqual()
+        {
+            // Arrange
+            var quote = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var comparer = new QuoteComparer();
+
+            // Act
+            int result = comparer.Compare(quote, quote);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Compare_SamePrice_SameVolume_ReturnsEqual()
+        {
+            // Arrange
+            var quoteA = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var quoteB = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var comparer = new QuoteComparer();
+
+            // Act
+            int result = comparer.Compare(quoteA, quoteB);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Compare_BetterPrice_ReturnsOrdered()
+        {
+            // Arrange
+            var quoteA = new Quote("TestSymbol", 2d, 10, DateTime.Now.AddDays(1));
+            var quoteB = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var comparer = new QuoteComparer();
+
+            // Act
+            int result = comparer.Compare(quoteA, quoteB);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Compare_WorsePrice_ReturnsUnordered()
+        {
+            // Arrange
+            var quoteA = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var quoteB = new Quote("TestSymbol", 2d, 10, DateTime.Now.AddDays(1));
+            var comparer = new QuoteComparer();
+
+            // Act
+            int result = comparer.Compare(quoteA, quoteB);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Compare_SamePrice_HigherVolume_ReturnsOrdered()
+        {
+            // Arrange
+            var quoteA = new Quote("TestSymbol", 5d, 20, DateTime.Now.AddDays(1));
+            var quoteB = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var comparer = new QuoteComparer();
+
+            // Act
+            int result = comparer.Compare(quoteA, quoteB);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Compare_SamePrice_LowerVolume_ReturnsUnordered()
+        {
+            // Arrange
+            var quoteA = new Quote("TestSymbol", 5d, 10, DateTime.Now.AddDays(1));
+            var quoteB = new Quote("TestSymbol", 5d, 20, DateTime.Now.AddDays(1));
+            var comparer = new QuoteComparer();
+
+            // Act
+            int result = comparer.Compare(quoteA, quoteB);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
